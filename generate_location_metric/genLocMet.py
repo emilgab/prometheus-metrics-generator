1dc3f77470f55d0d3f85905d216f4a77e9fc37aa
Emil Gabrielli <emilgabrielli@gmail.com>
2019-12-12 21:34:56 +0100

    Added the comments on each file
    These comments will explain the purpose of that particular script.y

---
+++ generate_location_metric/genLocMet.py
@@ -0,0 +1,7 @@
+# genLocMet.py
+# This contains the genLocMet() object that will be able to
+# generate the metrics for each city.
+
+class genLocMet():
+
+    pass
--- generate_metrics.py
+++ generate_metrics.py
@@ -1,2 +1,13 @@
 # File for generating the metrics for prometheus
 # This works as the main file which will use the genLocMet() object
+# This main file has a job, which is to create the metric file itself.
+# Which will look like this (exclude python comments):
+# TYPE store_metrics gauge
+# HELP store_metrics Contains the metrics read from each store
+# store_in_question_1{name="Store_name"} metric
+# store_in_question_2{name="Store_name"} metric
+# ...
+# ...
+
+# * imports *
+from generate_location_metric.genLocMet import genLocMet
